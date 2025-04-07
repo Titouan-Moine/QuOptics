693fe40d67823f5832c8468908250f6ceffb11eb
Moine Titouan <titouan.moine@student-cs.fr>
2025-04-07 14:58:56 +0200

    start of the branch

---
+++ tensor.py
@@ -0,0 +1,10 @@
+
+import numpy as np
+
+class Node:
+    def __init__(self, rank, dimensions, value, lines):
+        self.rank = rank
+        self.dimensions = dimensions
+        self.valu = value
+        self.lines = lines
+        
