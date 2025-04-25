24bec9105339036d874aef1031af06c45ff89422
Lechoux Gaetan <gaetan.lechoux@student-cs.fr>
2025-04-25 08:52:58 +0200

    classes of tensors

--- classes.py
+++ classes.py
@@ -1,3 +1,4 @@
+import numpy as np
 class Tensor:
   def __init__(self, dimension, content, name="Unnamed Tensor"):
     """
@@ -14,14 +15,15 @@ class Tensor:
   def __repr__(self):
     return f"Tensor(name={self.name}, dimension={self.dimension})"
 
-  def reshape(self, new_dimension):
-    """
-    Reshape the tensor to a new dimension.
+  # def reshape(self, new_dimension):
+  #   """
+  #   Reshape the tensor to a new dimension.
 
-    :param new_dimension: Tuple representing the new dimensions.
-    """
-    # Add logic to reshape content if necessary
-    self.dimension = new_dimension
+  #   :param new_dimension: Tuple representing the new dimensions.
+  #   """
+  #   # Add logic to reshape content if necessary
+  #   self.dimension = new_dimension
+
 
   def get_content(self):
     """
@@ -40,11 +42,32 @@ class Tensor:
     self.content = new_content
 
   @staticmethod
+  def empty_tenser(name="Empty Tensor"):
+    """
+    Create an empty tensor.
+      
+    :return: A Tensor object with no content.
+    """
+    return Tensor(dimension=(), content=None, name=name)
+
+    
   def ket_0():
     """
-    Create a specific tensor representing the quantum state |0>.
-
     :return: A Tensor object representing |0>.
     """
     ket_0_content = [[1], [0]]  # The vector (1, 0) as a tensor of shape (2, 1)
     return Tensor(dimension=(2, 1), content=ket_0_content, name="Ket 0")
+  
+  def ket_1():
+    return Tensor(dimension=(2, 1), content=[[0], [1]], name="Ket 1")   
+
+  def c_not():
+    """
+    :return: A Tensor object representing the CNOT gate. With control on bit 1 and target bit 2.
+    """
+    c_not_content = [[[[1, 0], [0, 0]], [[0, 0], [1, 0]]], [[[0, 0], [0, 1]], [[0, 1], [0, 0]]]]
+    return Tensor(dimension=(2, 2, 2, 2), content=c_not_content, name="CNOT")
+
+  def hadamard():
+    hadamard_content = [[1 / np.sqrt(2), 1 / np.sqrt(2)], [1 / np.sqrt(2), -1 / np.sqrt(2)]]
+    return Tensor(dimension=(2, 2), content=hadamard_content, name="Hadamard")
--- tenseurs.py
+++ tenseurs.py
@@ -1,5 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from classes import *
+
+
 def create_tenser(shape):
   return np.zeros(shape)
 def add_tensers(tenser1, tenser2):
@@ -22,32 +25,32 @@ def add_edge(graph, node1, node2, data=None):
 def get_node_data(graph, node):
   return graph.nodes[node]['data']
 
-def ket_0():
-  ket_0 = create_tenser((2, 1))
-  ket_0[0, 0] = 1
-  ket_0[1, 0] = 0
-  return ket_0
-def ket_1():
-  ket_1 = create_tenser((2, 1))
-  ket_1[0, 0] = 0
-  ket_1[1, 0] = 1
-  return ket_1
-def c_not():
-  c_not = create_tenser((2, 2, 2, 2))
-  c_not[0, 0, 0, 0] = 1
-  c_not[0, 0, 1, 1] = 1
-  c_not[1, 1, 1, 0] = 1
-  c_not[1, 1, 0, 1] = 1
+# def ket_0():
+#   ket_0 = create_tenser((2, 1))
+#   ket_0[0, 0] = 1
+#   ket_0[1, 0] = 0
+#   return ket_0
+# def ket_1():
+#   ket_1 = create_tenser((2, 1))
+#   ket_1[0, 0] = 0
+#   ket_1[1, 0] = 1
+#   return ket_1
+# def c_not():
+#   c_not = create_tenser((2, 2, 2, 2))
+#   c_not[0, 0, 0, 0] = 1
+#   c_not[0, 0, 1, 1] = 1
+#   c_not[1, 1, 1, 0] = 1
+#   c_not[1, 1, 0, 1] = 1
   
-  return c_not
-def hadamard():
-  hadamard = create_tenser((2, 2))
-  hadamard[0, 0] = 1 / np.sqrt(2)
-  hadamard[1, 1] = -1 / np.sqrt(2)
-  hadamard[0, 1] = 1 / np.sqrt(2)
-  hadamard[1, 0] = 1 / np.sqrt(2)
+#   return c_not
+# def hadamard():
+#   hadamard = create_tenser((2, 2))
+#   hadamard[0, 0] = 1 / np.sqrt(2)
+#   hadamard[1, 1] = -1 / np.sqrt(2)
+#   hadamard[0, 1] = 1 / np.sqrt(2)
+#   hadamard[1, 0] = 1 / np.sqrt(2)
   
-  return hadamard
+  # return hadamard
 def exemple():
   g=create_graph()
 
@@ -71,5 +74,30 @@ def exemple():
   node_colors=['red' if g.nodes[node]['data']==ket_0 else 'blue' if g.nodes[node]['data']==hadamard else 'green' if g.nodes[node]['data']==c_not else 'black' for node in g.nodes()]
   pos = nx.spring_layout(g)  # Positions for all nodes
   nx.draw(g, pos, with_labels=True, node_color=node_colors, node_size=1500, font_size=12, font_weight='bold')
-exemple()
+
+def exemple2():
+  g=create_graph()
+
+  ket_0=Tensor.ket_0()
+  hadamard=Tensor.hadamard()
+  c_not=Tensor.c_not()
+  empty_tenser=Tensor.empty_tenser()
+  add_node(g, "output1", empty_tenser)
+  add_node(g, "output2", empty_tenser)
+  add_node(g, "hadamard", hadamard)
+  add_node(g, "c_not", c_not)
+  add_node(g, "Qbit_0", ket_0)
+  add_node(g, "Qbit_1", ket_0)
+
+  add_edge(g, "Qbit_0", "hadamard")
+  add_edge(g, "hadamard", "c_not")
+  add_edge(g, "Qbit_1", "c_not")
+  add_edge(g, "c_not", "empty_tenser1")
+  add_edge(g, "c_not", "empty_tenser2")
+  node_colors=['red' if g.nodes[node]['data']==ket_0 else 'blue' if g.nodes[node]['data']==hadamard else 'green' if g.nodes[node]['data']==c_not else 'black' for node in g.nodes()]
+  pos = nx.spring_layout(g)  # Positions for all nodes
+  nx.draw(g, pos, with_labels=True, node_color=node_colors, node_size=1500, font_size=12, font_weight='bold')
+
+#exemple()
+exemple2()
 plt.show()
