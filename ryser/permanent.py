398f4d379da7c8b5b1a5ee2c9b03c15284c225cf
Moine Titouan <titouan.moine@student-cs.fr>
2025-11-26 21:26:41 +0100

    implemented my modified Ryser with and without mixed-radix Gray code

--- ryser/permanent.py
+++ ryser/permanent.py
@@ -9,17 +9,21 @@ Key Functions :
     - ryser_gray: Compute the permanent using Gray code optimization
     - ryser_hyperrect: Compute the permanent of a repeating sub-matrix using an
         ameliorated Ryser's algorithm
-    - mixed_gray : Generate a sort of Gray code for mixed-radix numbers
-    - ryser_hyperrect_mixed: Compute the permanent of a repeating sub-matrix using
+    - gray_mixed : Generate a sort of Gray code for mixed-radix numbers
+    - ryser_hyperrect_gray: Compute the permanent of a repeating sub-matrix using
         mixed-radix Gray code optimization
 
 """
 import sys
 import os
+from math import comb
+from itertools import product
+import numpy as np
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(parent_dir)
-import numpy as np
 from clements_scheme.rnd_unitary import random_unitary
+
+
 
 
 
@@ -124,3 +128,177 @@ def ryser_gray(A):
     return perm
 
 
+
+def ryser_hyperrect(U, vecn, vecm, n=None):
+    """Compute the permanent of a repeating sub-matrix using an ameliorated Ryser's algorithm.
+
+    Parameters
+    ----------
+    U : np.ndarray
+        The base matrix of shape (N, N).
+    n : np.ndarray
+        An array containing the row multiplicities.
+    m : np.ndarray
+        An array containing the column multiplicities.
+    
+    Returns
+    -------
+    complex
+        The permanent of the repeating sub-matrix.
+        
+    """
+    if n is None:
+        n = np.sum(vecn)
+    if np.sum(vecn) != np.sum(vecm) or np.sum(vecm) != n:
+        raise ValueError("vecn and vecm must sum to the same amount\
+                         (i.e. have the same number of photons)")
+    N = U.shape[0]
+    # take indices of non-zero coords of vecn and vecm
+    nzn_mask = vecn != 0
+    nzm_mask = vecm != 0
+    nzn_index = np.arange(N)[nzn_mask]
+    nzm_index = np.arange(N)[nzm_mask]
+    nzn = vecn[nzn_mask]
+    nzm = vecm[nzm_mask]
+    #base_index = np.searchsorted(nzm_index,)
+    binom_coef = {(j,c): comb(nzm[j], c) for j in range(len(nzm)) for c in range(nzm[j]+1)}
+    perm = 0
+
+    for c in product(*(range(vecm[j]+1) for j in nzm_index)):
+        binom_prod = np.prod([binom_coef[(nzm_index[j], c[j])]
+                               for j in range(len(nzm))])
+        prod = np.prod([pow(np.sum([c[j]*U[i, nzm_index[j]]
+                                    for j in range(len(nzm))]), nzn[i]) for i in range(len(nzn))])
+        sign = (-1)**(n-np.sum(c))
+        
+        perm += sign * binom_prod * prod
+    
+    return perm
+
+def gray_mixed(radix, prefix=(), reversed=False):
+    """Generate a mixed-radix Gray code for given radices
+
+    Parameters
+    ----------
+    radix : np.ndarray
+        An array containing the radices.
+
+    Yields
+    ------
+    tuple of: (np.ndarray, int, int)
+        Contains in order:
+        - The next vector in gray code order
+        - the index of the change
+        - the sign of the change.
+    """
+    if radix.shape[0] == 0:
+        yield np.array(prefix)
+    else:
+        if reversed:
+            for c in range(radix[0], -1, -1):
+                if c % 2 == 0:
+                    yield from gray_mixed(radix[1:], prefix + (c,), reversed=True)
+                else:
+                    yield from gray_mixed(radix[1:], prefix + (c,))
+        else:
+            for c in range(radix[0]+1):
+                if c % 2 == 0:
+                    yield from gray_mixed(radix[1:], prefix + (c,))
+                else:
+                    yield from gray_mixed(radix[1:], prefix + (c,), reversed=True)
+
+def index_and_sign(c, prev_c):
+    """Determine the index and sign of the change between two mixed-radix gray vectors.
+
+    Parameters
+    ----------
+    c : np.ndarray
+        The current vector.
+    prev_c : np.ndarray
+        The previous vector.
+    
+    Returns
+    -------
+    tuple of: (int, int)
+        Contains in order:
+        - the index of the change
+        - the sign of the change.
+    """
+    diff_idx = np.nonzero(c != prev_c)[0]
+    #print(diff_idx)
+    if len(diff_idx) != 1:
+        raise ValueError("c and prev_c must differ by exactly one element")
+    idx = diff_idx[0]
+    sign = int(c[idx] - prev_c[idx])
+    return idx, sign
+
+
+def ryser_hyperrect_gray(U, vecn, vecm, n=None):
+    """Compute the permanent of a repeating sub-matrix using an ameliorated Ryser's algorithm
+    and mixed-radix gray order.
+
+    Parameters
+    ----------
+    U : np.ndarray
+        The base matrix of shape (N, N).
+    n : np.ndarray
+        An array containing the row multiplicities.
+    m : np.ndarray
+        An array containing the column multiplicities.
+    
+    Returns
+    -------
+    complex
+        The permanent of the repeating sub-matrix.
+        
+    """
+    if n is None:
+        n = np.sum(vecn)
+    if np.sum(vecn) != np.sum(vecm) or np.sum(vecm) != n:
+        raise ValueError("vecn and vecm must sum to the same amount\
+                         (i.e. have the same number of photons)")
+
+    N = U.shape[0]
+    # take indices of non-zero coords of vecn and vecm
+    nzn_mask = vecn != 0
+    nzm_mask = vecm != 0
+    nzn_index = np.arange(N)[nzn_mask]
+    nzm_index = np.arange(N)[nzm_mask]
+    nzn = vecn[nzn_mask]
+    nzm = vecm[nzm_mask]
+    
+    sign = (-1)**n
+    prev_c = np.zeros(len(nzm), dtype=int)
+    binom_prod = 1
+    row_sum = np.zeros(len(nzn), dtype=complex)
+    prod = 1
+    perm = 0.0 + 0.0j
+    
+    first = True
+    for c in gray_mixed(nzm):
+        if first:
+            first = False
+        else:
+            diff_idx, diff_sign = index_and_sign(c, prev_c)
+            changed_c = c[diff_idx]
+            if diff_sign > 0:
+                binom_prod = binom_prod * nzm[diff_idx] - changed_c + 1 / changed_c
+                row_sum += U[nzn_index, nzm_index[diff_idx]]
+            else:
+                binom_prod = binom_prod * changed_c + 1 / (nzm[diff_idx] - changed_c)
+                row_sum -= U[nzn_index, nzm_index[diff_idx]]
+
+            prod = np.prod([pow(row_sum[i], nzn[i]) for i in range(len(nzn))])
+            sign = - sign
+            perm += sign * binom_prod * prod
+            prev_c = c
+    return perm
+
+
+U = random_unitary(4)
+vecn = np.array([1,1,1,1])
+vecm = np.array([1,1,1,1])
+print("Permanent (Ryser) :", ryser(U))
+print("Permanent (Ryser Gray) :", ryser_gray(U))
+print("Permanent (Ryser Hyperrect) :", ryser_hyperrect(U, vecn, vecm))
+print("Permanent (Ryser Hyperrect Gray) :", ryser_hyperrect_gray(U, vecn, vecm))
