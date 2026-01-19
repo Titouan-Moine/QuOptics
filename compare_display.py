e10197b42cb717b64fc48cb583ecfea0a2b3236b
Lechoux Gaetan <gaetan.lechoux@student-cs.fr>
2026-01-19 16:28:19 +0100

    test dans fock

--- compare_display.py
+++ compare_display.py
@@ -8,7 +8,7 @@ import fock_amplitude as fa
 #from fock_amplitude import clements_fock_tensor
 # Configuration for benchmarking
 def transformationA(U):
-  return sm.contract_circuit(U)
+  return sm.contract_circuit_then_fock(U)
 
 def transformationB(U):
   a,b=U
@@ -28,9 +28,7 @@ def main():
 
   # Benchmarking loop
   for size in circuit_sizes:
-      # times_A = []
-      # times_B = []
-      
+     
       # Generate a random unitary matrix
       U = random_unitary(size)
       
@@ -51,39 +49,6 @@ def main():
       elapsed_time_B = time.time() - start_time
       times_B.append(elapsed_time_B)
           
-      
-  #     # Calculate average times and standard deviations (filtering out None values)
-  #     valid_times_A = [t for t in times_A if t is not None]
-  #     valid_times_B = [t for t in times_B if t is not None]
-      
-  #     avg_time_A = np.mean(valid_times_A) if valid_times_A else None
-  #     avg_time_B = np.mean(valid_times_B) if valid_times_B else None
-      
-  #     std_time_A = np.std(valid_times_A) if valid_times_A else None
-  #     std_time_B = np.std(valid_times_B) if valid_times_B else None
-      
-  #     # Store results for later plotting and analysis
-  #     results_transfo_A['sizes'].append(size)
-  #     results_transfo_A['times'].append(avg_time_A)
-  #     results_transfo_A['errors'].append(std_time_A)
-      
-  #     results_transfo_B['sizes'].append(size)
-  #     results_transfo_B['times'].append(avg_time_B)
-  #     results_transfo_B['errors'].append(std_time_B)
-
-
-  # # Sanitize results: replace None with NaN for times, and 0.0 for errors
-  # sizes_A = results_transfo_A['sizes']
-  # times_A = [t if t is not None else np.nan for t in results_transfo_A['times']]
-  # errs_A  = [e if e is not None else 0.0   for e in results_transfo_A['errors']]
-
-  # sizes_B = results_transfo_B['sizes']
-  # times_B = [t if t is not None else np.nan for t in results_transfo_B['times']]
-  # errs_B  = [e if e is not None else 0.0   for e in results_transfo_B['errors']]
-
-  # plt.errorbar(sizes_A, times_A, yerr=errs_A, fmt='-o', color='blue', label='transfo_A', capsize=4)
-  # plt.errorbar(sizes_B, times_B, yerr=errs_B, fmt='-o', color='red',  label='transfo_B', capsize=4)
-  
   plt.plot(circuit_sizes,times_A, color='blue')
   plt.plot(circuit_sizes,times_B, color='red')
   plt.show()
